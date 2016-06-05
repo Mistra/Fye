4b799873820857731f4c4ae913b509c1ca5b1796
Giulio M <sperectrum@gmail.com>
2016-06-06 00:16:48 +0200

    implemented base db connection

--- web/index.php
+++ web/index.php
@@ -3,7 +3,51 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
-require './person.php';
+require_once './person.php';
+
+interface Dao {
+    public function select();
+    //public function selectOne($id);
+    //public function insert($object);
+}
+
+class HelperDao implements Dao {
+    function __construct() {
+        $this->host = "ec2-54-243-203-143.compute-1.amazonaws.com";
+        $this->db = "dei6cvjl0ekm7s";
+        $this->user = "wskvnrlsywglmk";
+        $this->password = "qHjkBDEnqsU9rAAN1Zji94V608";
+    }
+
+    function connect() {
+        return new PDO("pgsql:dbname=$this->db;host=$this->host", $this->user, $this->password);
+    }
+
+    function select() {
+        $helperList = array();
+        try {
+            $conn = $this->connect();
+            //$statement = $conn->prepare("select id from some_table where name = :name");
+            $statement = $conn->prepare("select * from helper");
+            //$statement->execute(array(':name' => "Jimbo"));
+            $statement->execute();
+
+            foreach ($statement->fetchAll() as $row) {
+                $helperList[] = new person(
+                $row["name"],
+                $row["surname"],
+                $row["email"],
+                $row["nation"],
+                $row["country"],
+                $row["faculty"]);
+            }
+        } //catch(PDOException $e)
+        finally {
+            $conn=null;
+        }
+        return $helperList;
+    }
+}
 
 $app = new \Slim\App;
 $container = $app->getContainer();
@@ -23,20 +67,23 @@ $app->get('/api/testJson/filter/{value}', function (Request $request, Response $
     $peopleArray = array();
     $peopleArray[] = $var;
     $peopleArray[] = $var;
-    $response->getBody()->write(json_encode($peopleArray, JSON_FORCE_OBJECT));
+    $response->getBody()->write(json_encode($peopleArray));
 
     return $response;
 });
 
 $app->get('/api/testJson/filter', function (Request $request, Response $response) {
-    //$name = $request->getAttribute('value');
+    $helperDao = new HelperDao;
+    $helperList = $helperDao->select();
+    $response->getBody()->write(json_encode($helperList));
 
-    $var = new person("gino", "pini", "pini@email", "Italia", "Parma", "biologia");
-    $var2 = new person("gino", "pini", "pin@email", "Italia", "Parma", "biologia");
-    $peopleArray = array();
-    $peopleArray[] = $var;
-    $peopleArray[] = $var2;
-    $response->getBody()->write(json_encode($peopleArray));
+    return $response;
+});
+
+$app->get('/api/test', function (Request $request, Response $response) {
+    $helperDao = new HelperDao;
+    $result = $helperDao->select();
+    $response->getBody()->write(json_encode($result));
 
     return $response;
 });
@@ -45,7 +92,6 @@ $app->get('/', function (Request $request, Response $response) {
     //$this->logger->addInfo("Ticket list");
     //$mapper = new TicketMapper($this->db);
     //$tickets = $mapper->getTickets();
-
     $response = $this->view->render($response, "index.html");
     return $response;
 });
