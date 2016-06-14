f6c639c09ee2fc0460478f724ac989ee406866c9
Giulio M <sperectrum@gmail.com>
2016-06-14 18:45:33 +0200

    Fixed Menu

--- webapp/app/menuHeader.js
+++ webapp/app/menuHeader.js
@@ -1,20 +1,24 @@
 import React from 'react';
 
+class MenuRow extends React.Component {
+    render() {
+        return (
+            <li class={this.props.active + " nav-item"}>
+                <a class="nav-link" href="#">{this.props.name} <span class="sr-only">(current)</span></a>
+            </li>
+        );
+    }
+}
+
 export default class MenuHeader extends React.Component {
     render() {
         return (
             <nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
-
-                    <a class="navbar-brand" href="#">Navbar</a>
+                    <a class="navbar-brand" href="#">Fye</a>
                     <ul class="nav navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">{localStorage.getItem("lastname")}</a>
-                        </li>
+                        <MenuRow active="active" name = "Helpers" />
+                        <MenuRow active="" name = "Outgoing" />
                     </ul>
-
             </nav>
         );
     }
